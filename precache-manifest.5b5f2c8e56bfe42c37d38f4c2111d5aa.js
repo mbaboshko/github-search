785dff79c04b1323bb13f4c44898d0412a7745cc
Maksym Baboshko <maksym.baboshko@yahoo.com>
2020-08-17 20:13:05 +0300

    Updates

---
+++ precache-manifest.5b5f2c8e56bfe42c37d38f4c2111d5aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03496b8c36bf38b8b1cd4e68a963512f",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "5de86cd007b3050d16e1",
+    "url": "/github-search/static/css/main.7bd66c88.chunk.css"
+  },
+  {
+    "revision": "bf4e3cbcf9f3ad9c38a5",
+    "url": "/github-search/static/js/2.b7be214e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-search/static/js/2.b7be214e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de86cd007b3050d16e1",
+    "url": "/github-search/static/js/main.a9d9e138.chunk.js"
+  },
+  {
+    "revision": "fe3e4ffa84f84f83a680",
+    "url": "/github-search/static/js/runtime-main.6ac03fc8.js"
+  }
+]);
